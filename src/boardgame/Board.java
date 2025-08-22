ef865105851df22d410df473c28361b809a74b61
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-08-22 17:16:20 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -21,11 +21,11 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	
+
 	public int getColumns() {
 		return columns;
 	}
-	
+
 	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
@@ -40,12 +40,25 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
-	public void placepiece(Piece piece, Position position) {
+	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 
 	private boolean positionExists(int row, int column) {
@@ -55,7 +68,7 @@ public class Board {
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
-	
+
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
